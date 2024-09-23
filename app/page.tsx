cdfb18c23dab572552cb33cfa8f7381b9e4d505e
Faraz Alam <153891083+faraz141@users.noreply.github.com>
2024-09-23 23:53:33 +0500

    create this app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center bg-black opacity-85 h-screen dark:bg-black">
+      <h1 className="text-center text-6xl bg-gradient-to-r from-blue-500 via-red-600 to-purple-500 bg-clip-text text-transparent">
+        Hello World
+      </h1>
+    </div>
+  );
+}
